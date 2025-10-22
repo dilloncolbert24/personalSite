0a3ff34150f64a2efdc481537d6f853e8cb1efde
Dillon Colbert <dillon@DillLaptop.local>
2025-10-21 21:17:47 -0500

    stupid ass lowercase

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from 'react';
-import headshot from './baywheels_image.webp';
+import headshot from './bayWheels_image.webp';
 
 const MODE_OPTIONS = ['Walk', 'Bike', 'Transit', 'Car', 'Rideshare', 'Other'];
 
